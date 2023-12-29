617da6b5931e871ac28d4448d8c483e9e9d88eb9
Juan Carlos Sigler Priego <juan@repl.it>
2023-12-29 10:56:35 -0600

    v2

---
+++ src/client/pages/Main.tsx
@@ -0,0 +1,11 @@
+import { Container } from "../components/twcss/Container";
+
+const Main = () => {
+  return (
+    <Container twCss={{ margin: "m-24", backgroundColor: "bg-gray-950", borderRadius: "rounded-3xl", padding: "p-8" }}>
+      Hello World!
+    </Container>
+  );
+};
+
+export default Main;
---
+++ src/server/routes/api/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { Router } from "express";
+import errorHandlingMiddleware from "../../middleware/errorHandling";
+
+const router = Router();
+
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+
+// TODO: Add routes here
+router.get("/test", (_req, res) => {
+    res.json({ msg: "Hello World" });
+});
+
+router.use(errorHandlingMiddleware());
+
+export const provide = () => router;
