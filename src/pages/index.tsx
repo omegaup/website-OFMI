d3e16403567d3a7c4eeddb3bf0548aa1ea4d8d22
Javier Pantoja <javierjpc1305@gmail.com>
2023-12-29 17:05:18 -0600

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,4 @@
-import { Container } from '@/components/twcss/Container'
-import { Text } from '@/components/twcss/Text'
 
 export default function Home (): JSX.Element {
-  return (
-    <Container twCss={{
-      backgroundColor: 'bg-emerald-950'
-    }}
-    >
-      <Text>Hello world!</Text>
-    </Container>
-  )
+  return <></>
 }
