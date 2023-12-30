7d27b20a0aa6d2db8183bb017dfa42fd81e650c7
Juan Carlos Sigler Priego <juanito@omegaup.com>
2023-12-30 11:22:54 -0600

    Remove babel (#30)
    * eslint + prettier
    
    * both
    
    * fix
    
    * remove babel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    swcPlugins: [["@swc-jotai/react-refresh", {}]],
+  },
 };
 
 module.exports = nextConfig;
