293c43ae23eaf4f6a24efc2f7dd2cd773329cd60
Juan Carlos Sigler Priego <juan@repl.it>
2023-12-29 22:23:58 -0600

    Fix tests resolver

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,6 +1,6 @@
 import { expect, test } from 'vitest'
 import { render } from '@testing-library/react'
-import Page from '../src/pages'
+import Page from '@/pages'
 
 test('Page', () => {
   render(<Page />)
