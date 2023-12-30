23526033d0fbea1372d638a29cb4e982c13056a3
Javier Pantoja <javierjpc1305@gmail.com>
2023-12-29 18:22:20 -0600

    button component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Button } from '@/components/button'
 
 const navigation = [
   { name: 'Dashboard', href: '#', current: true },
---
+++ src/types/components.types.ts
@@ -0,0 +1,1 @@
+export type Size = 'sx' | 'sm' | 'base' | 'lg' | 'xl'
