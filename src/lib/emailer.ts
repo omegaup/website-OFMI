a22e33ea800df5d081266bbb4b1ed3b0fc7a229c
Andrea Santillana <asantillanafernandez@netflix.com>
2023-12-30 13:39:39 -0600

    fix linter

--- src/lib/emailer.ts
+++ src/lib/emailer.ts
@@ -1,36 +1,7 @@
-
 import * as nodemailer from "nodemailer";
 import { MailOptions } from "nodemailer/lib/json-transport";
 
-export class Emailer {
-  private readonly transporter: nodemailer.Transporter;
-
-  constructor() {
-    this.transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.OFMI_EMAIL_SMTP_USER,
-        pass: process.env.OFMI_EMAIL_SMTP_PASSWORD
-      },
-    });
-  }
-
-  public sendEmail(mailOptions: MailOptions) {
-    return this.transporter.sendMail(mailOptions);
-  }
-
-  public notifyUserForSignup(email: string) {
-    this.sendEmail(newUserEmailTemplate(email));
-  }
-
-  public notifyUserSuccessfulSignup(email: string) {
-    this.sendEmail(signUpSuccessfullEmailTemplate(email));
-  }
-}
-
-export const emailer = new Emailer();
-
-export const newUserEmailTemplate = (email: string) => {
+export const newUserEmailTemplate = (email: string): MailOptions => {
   return {
     from: process.env.OFMI_EMAIL_SMTP_USER,
     to: email,
@@ -48,13 +19,13 @@ export const newUserEmailTemplate = (email: string) => {
   } as MailOptions;
 };
 
-export const signUpSuccessfullEmailTemplate = (email: string) => {
-    return {
-      from: process.env.OFMI_EMAIL_SMTP_USER,
-      to: email,
-      subject: "La página de la OFMI te da la bienvenida",
-      text: "La página de la OFMI te da la bienvenida",
-      html: `
+export const signUpSuccessfullEmailTemplate = (email: string): MailOptions => {
+  return {
+    from: process.env.OFMI_EMAIL_SMTP_USER,
+    to: email,
+    subject: "La página de la OFMI te da la bienvenida",
+    text: "La página de la OFMI te da la bienvenida",
+    html: `
         <p>Ahora ya tienes un login para usar la página de la OFMI.</p>
         <p>Es muy importante que recuerdes que este login no significa que ya estés participando en la OFMI, para ese registro tienes que ir a la liga de 
         <a href="https://ofmi.omegaup.com/registro">Registro</a> </p>
@@ -63,6 +34,33 @@ export const signUpSuccessfullEmailTemplate = (email: string) => {
         <br />
         <p>Equipo organizador de la OFMI</p>
       `,
-    } as MailOptions;
-  };
-  
+  } as MailOptions;
+};
+
+export class Emailer {
+  private readonly transporter: nodemailer.Transporter;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.OFMI_EMAIL_SMTP_USER,
+        pass: process.env.OFMI_EMAIL_SMTP_PASSWORD,
+      },
+    });
+  }
+
+  public sendEmail(mailOptions: MailOptions): Promise<void> {
+    return this.transporter.sendMail(mailOptions);
+  }
+
+  public notifyUserForSignup(email: string): void {
+    this.sendEmail(newUserEmailTemplate(email));
+  }
+
+  public notifyUserSuccessfulSignup(email: string): void {
+    this.sendEmail(signUpSuccessfullEmailTemplate(email));
+  }
+}
+
+export const emailer = new Emailer();
