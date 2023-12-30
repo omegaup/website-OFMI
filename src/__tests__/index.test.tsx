9a8cc40f498bacbeb0b120a8b26e0532e468f1f0
Juan Carlos Sigler Priego <juan@repl.it>
2023-12-30 12:58:33 -0600

    Add tests

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,8 +1,13 @@
 import { expect, test } from "vitest";
 import { render } from "@testing-library/react";
 import Page from "@/pages";
+import { SessionProvider } from "next-auth/react";
 
 test("Page", () => {
-  render(<Page />);
+  render(
+    <SessionProvider session={null}>
+      <Page />
+    </SessionProvider>,
+  );
   expect(true, "dummy test success!");
 });
