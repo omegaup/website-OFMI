5feff9a4a18f3fbfd3e92112512fc6265ed503ad
Juan Carlos Sigler Priego <juan@repl.it>
2023-12-30 01:00:04 -0600

    redirect

--- src/components/signup/index.tsx
+++ src/components/signup/index.tsx
@@ -36,8 +36,12 @@ export default function SignUp(): JSX.Element {
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img className="mx-auto h-16 w-auto" src="/logo.svg" alt="OFMI" />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          <img
+            className="mx-auto mt-8 h-28 w-auto"
+            src="/logo.svg"
+            alt="OFMI"
+          />
+          <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Crea una cuenta
           </h2>
         </div>
