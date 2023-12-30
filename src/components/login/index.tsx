ecf9b1b228d86866135b9cd8caf318927ed6412e
Juan Carlos Sigler Priego <juan@repl.it>
2023-12-30 01:05:41 -0600

    iniciar sesion

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,19 +1,15 @@
 export default function Login(): JSX.Element {
   return (
     <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-white">
-        <body class="h-full">
-        ```
-      */}
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img className="mx-auto h-16 w-auto" src="/logo.svg" alt="OFMI" />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
+          <img
+            className="mx-auto mt-8 h-28 w-auto"
+            src="/logo.svg"
+            alt="OFMI"
+          />
+          <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Iniciar sesión
           </h2>
         </div>
 
@@ -72,7 +68,7 @@ export default function Login(): JSX.Element {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Sign in
+                Iniciar sesión
               </button>
             </div>
           </form>
