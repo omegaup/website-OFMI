46e0a099f890a8ff999fcb179a9a602753fe901c
Freddy Román <me@freddy.mx>
2023-12-30 21:09:02 -0800

    ha

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -3,10 +3,7 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { ProfileDropdown } from "./Profile";
 import { Unauthenticated } from "./Unauthenticated";
 import { classNames } from "./styles";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
-import { useEffect, useState } from "react";
-import { Session } from "next-auth";
+import { useSession } from "next-auth/react";
 
 const navigation = {
   Material: "/material",
@@ -21,14 +18,7 @@ export const Navbar = ({
 }: {
   activeItem?: NavigationItem;
 }): JSX.Element => {
-  const [session, setSession] = useState<Session>();
-  useEffect(() => {
-    async function getSessionAsync(): Promise<void> {
-      const s = (await getServerSession(authOptions)) ?? undefined;
-      setSession(s);
-    }
-    getSessionAsync();
-  }, []);
+  const session = useSession();
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }: { open: boolean }) => (
@@ -73,7 +63,7 @@ export const Navbar = ({
                   </div>
                 </div>
               </div>
-              {session?.user != null ? (
+              {session?.data != null ? (
                 <ProfileDropdown />
               ) : (
                 <Unauthenticated />
