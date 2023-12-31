af78fe6438f03b9c1202d4c68ae14c8caf06a84c
Andrea Santillana <asantillanafernandez@netflix.com>
2023-12-30 22:24:05 -0600

    verify email

--- src/lib/emailer.ts
+++ src/lib/emailer.ts
@@ -1,16 +1,16 @@
 import * as nodemailer from "nodemailer";
 import { MailOptions } from "nodemailer/lib/json-transport";
 
-export const newUserEmailTemplate = (email: string): MailOptions => {
+export const newUserEmailTemplate = (email: string, url: string): MailOptions => {
   return {
     from: process.env.OFMI_EMAIL_SMTP_USER,
     to: email,
-    subject: "La página de la OFMI te da la bienvenida",
-    text: "La página de la OFMI te da la bienvenida",
+    subject: "Verifica tu cuenta de la página de la OFMI",
+    text: "Verifica tu cuenta de la página de la OFMI",
     html: `
-      <p>Ahora ya tienes un login para usar la página de la OFMI.</p>
-      <p>Es muy importante que recuerdes que este login no significa que ya estés participando en la OFMI, para ese registro tienes que ir a la liga de 
-      <a href="https://ofmi.omegaup.com/registro">Registro</a> </p>
+      <p>Gracias por registrarte en la página de la OFMI.</p>
+      <p>Por favor da click en <a href="${url}">esta liga</a> para verificar 
+      tu cuenta y que puedas empezar a usar la página </p>
       <p>Si tienes alguna duda por favor envía un correo a
       <a href="mailto:ofmi@omegaup.com">ofmi@omegaup.com</a></p>
       <br />
@@ -54,8 +54,8 @@ export class Emailer {
     return this.transporter.sendMail(mailOptions);
   }
 
-  public notifyUserForSignup(email: string): void {
-    this.sendEmail(newUserEmailTemplate(email));
+  public notifyUserForSignup(email: string, url: string): void {
+    this.sendEmail(newUserEmailTemplate(email, url));
   }
 
   public notifyUserSuccessfulSignup(email: string): void {
---
+++ src/pages/api/[token].ts
@@ -0,0 +1,57 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import {
+    VerifyEmailResponse,
+} from "@/types/auth.schema";
+import { BadRequestError } from "@/types/badRequestError.schema";
+import jwt from 'jsonwebtoken';
+import { prisma } from "@/lib/prisma";
+import { Prisma } from "@prisma/client";
+
+export default async function handle(
+    req: NextApiRequest,
+    res: NextApiResponse<VerifyEmailResponse | BadRequestError>,
+): Promise<void> {
+    if (req.method === "GET") {
+        await verifyEmailHandler(req, res);
+    } else {
+        return res.status(405).json({ message: "Method Not allowed" });
+    }
+}
+
+async function verifyEmailHandler(
+    req: NextApiRequest,
+    res: NextApiResponse,
+): Promise<void> {
+    const { token } = req.query;
+    try {
+        const result = jwt.verify(token as string, process.env.VERIFICATION_EMAIL_SECRET as string);
+        if (typeof result === 'string') {
+            throw new Error(result);
+        }
+        const user = await prisma.userAuth.findFirstOrThrow({
+            where: {
+                id: result.user
+            }
+        });
+
+        if (user.emailVerified != null) {
+            throw res.status(400).json({ message: "Este correo ya fue verificado previamente, ya puedes hacer login" });
+        }
+
+        await prisma.userAuth.update({
+            where: {
+                id: user.id
+            },
+            data: {
+                emailVerified: new Date()
+            }
+        });
+    } catch (e) {
+        if (e instanceof jwt.TokenExpiredError) {
+            return res.status(401).json({ message: "El token ha expirado, por favor solicita uno nuevo" });
+        }
+        return res.status(500).json({ message: "Internal server error" });
+    }
+
+    res.redirect(`${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/login`);
+}
