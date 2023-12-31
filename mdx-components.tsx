456cb8bc913b31f7acc2993ce14879f75d5fd785
Freddy Román <me@freddy.mx>
2023-12-30 20:42:50 -0800

    ugh

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -4,7 +4,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     wrapper: ({ children }) => (
       <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-8 lg:py-12">
-        <article className="prose prose-slate lg:prose-lg mx-auto mt-8">
+        <article className="prose prose-slate mx-auto mt-8 lg:prose-lg">
           {children}
         </article>
       </div>
