22217840b780dd0505f672f4ae1e9669c8fad7bf
Freddy Román <me@freddy.mx>
2023-12-30 20:40:07 -0800

    css

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,6 +2,13 @@ import type { MDXComponents } from "mdx/types";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    wrapper: ({ children }) => (
+      <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-8 lg:py-12">
+        <article className="prose prose-slate lg:prose-lg mx-auto mt-8">
+          {children}
+        </article>
+      </div>
+    ),
     ...components,
   };
 }
--- src/pages/egoi/index.mdx
+++
@@ -1,6 +0,0 @@
----
-layout: page
-title: Convocatoria a la 2a Olimpiada Femenil Mexicana de Informática
-navtitle: EGOI 2022
-redirect_to: /egoi/2022
----
