06eb7569c6e5faa1c6df1a1237c7560977724631
Freddy Román <me@freddy.mx>
2023-12-30 16:23:04 -0800

    Static pages, server-side rendering for navbar (#39)
    * initial static page import
    
    * more static
    
    * ok
    
    * idk
    
    * format
    
    * merge
    
    * server-side component
    
    * server-side
    
    * oops
    
    * bye

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,16 @@ import "@/styles/globals.css";
 import { Provider } from "jotai";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <SessionProvider session={pageProps.session}>
+      <Head>
+        <title>OFMI</title>
+      </Head>
       <Provider>
-        <Component {...pageProps} />;
+        <Component {...pageProps} />
       </Provider>
     </SessionProvider>
   );
