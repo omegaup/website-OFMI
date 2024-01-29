5479c8d0920a65e34090ccf61d3c59db7ba28b30
Aaron Diaz <73670693+aaron-diaz@users.noreply.github.com>
2024-01-28 21:40:05 -0600

    Fixing formatting errors

--- src/pages/registro.tsx
+++ src/pages/registro.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import ShippingDetails from "@/components/Forms/ShippingDetails";
 import PersonalDetails from "@/components/Forms/PersonalDetails";
+import { Response } from "@/components/Form/Form";
 import { ObjVals } from "@/types/input.types";
 
-export default function () {
+export default function Registro(): JSX.Element {
   const [collectShippingDetails, setCollectShippingDetails] = useState(true);
-  const submitPersonalDetails = (vals: ObjVals) => {
+  const submitPersonalDetails = (vals: ObjVals): Response => {
     if (vals.TyC === "No") {
       return {
         isError: true,
