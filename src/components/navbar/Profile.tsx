919b36856ea53e5b84903aa5edf66a8717b3f5f8
Juan Carlos Sigler Priego <jcsiglerp@gmail.com>
2024-08-18 02:53:41 -0400

    Profile image

--- src/components/navbar/Profile.tsx
+++ src/components/navbar/Profile.tsx
@@ -1,4 +1,6 @@
+import ProfileImageData from "public/profile.png";
 import { classNames } from "./styles";
+import Image from "next/image";
 
 import { Menu, Transition } from "@headlessui/react";
 import { signOut } from "next-auth/react";
@@ -12,11 +14,7 @@ export const ProfileDropdown = (): JSX.Element => {
           <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
             <span className="absolute -inset-1.5" />
             <span className="sr-only">Open user menu</span>
-            <img
-              className="h-8 w-8 rounded-full"
-              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              alt=""
-            />
+            <Image className="h-8 w-8 " src={ProfileImageData} alt="" />
           </Menu.Button>
         </div>
         <Transition
