3017a41bbbea310bf2661b59d147dbba5b388adb
Juan Carlos Sigler Priego <juanito@omegaup.com>
2024-09-09 01:34:28 -0400

    Estetica (#71)
    * Estetica
    
    * links bellos

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,40 @@
 import type { MDXComponents } from "mdx/types";
 
+function CustomH1({
+  children,
+}: React.HTMLAttributes<HTMLHeadElement>): JSX.Element {
+  return <h1 className="text-center">{children}</h1>;
+}
+
+function CustomP({
+  children,
+}: React.HTMLAttributes<HTMLParagraphElement>): JSX.Element {
+  return <p className="text-justify">{children}</p>;
+}
+
+function CustomLi({
+  children,
+}: React.HTMLAttributes<HTMLLIElement>): JSX.Element {
+  return <li className="text-justify">{children}</li>;
+}
+
+function CustomA({
+  children,
+  ...props
+}: React.AnchorHTMLAttributes<HTMLAnchorElement>): JSX.Element {
+  return (
+    <a className="text-justify hover:text-blue-700" {...props}>
+      {children}
+    </a>
+  );
+}
+
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    h1: CustomH1,
+    p: CustomP,
+    li: CustomLi,
+    a: CustomA,
     wrapper: ({ children }) => (
       <div className="relative flex min-h-screen flex-col overflow-hidden bg-gray-50 py-8 lg:py-12">
         <article className="prose prose-slate mx-auto mt-8 lg:prose-lg">
