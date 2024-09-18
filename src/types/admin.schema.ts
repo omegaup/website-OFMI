c584b1d3a52350e67ebc58ae0083ec6d19f5fe8e
dsantif <dianasantillana24@gmail.com>
2024-09-17 20:53:18 -0600

    descripción de endpoints

--- src/types/admin.schema.ts
+++ src/types/admin.schema.ts
@@ -6,9 +6,14 @@ export const SendEmailResponseSchema = Type.Object({
 });
 
 export type SendEmailRequest = Static<typeof SendEmailRequestSchema>;
-export const SendEmailRequestSchema = Type.Object({
-  email: Type.String(),
-  subject: Type.String({ minLength: 1 }),
-  // Html of the email content
-  content: Type.String({ minLength: 1 }),
-});
+export const SendEmailRequestSchema = Type.Object(
+  {
+    email: Type.String(),
+    subject: Type.String({ minLength: 1 }),
+    // Html of the email content
+    content: Type.String({ minLength: 1 }),
+  },
+  {
+    description: "Envía un correo desde la cuenta de ofmi-no-reply",
+  },
+);
