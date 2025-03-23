1f3ed9d3d84d3614d32881262cee58dae60d1479
Andrea Santillana <asantillanafernandez@netflix.com>
2025-03-23 14:39:28 -0600

    fix disabled paths

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,7 +12,7 @@ import {
 } from "@/lib/auth";
 
 // Prefix routes that are temporarily disabled
-const disabledPaths = [""];
+const disabledPaths: string[] = [];
 
 // Prefix routes that requires only to be log in
 const withAuthPaths = [
