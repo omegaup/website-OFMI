20851de89414308a8d926c452103459152549cf4
josephy <josephynimoto@gmail.com>
2026-02-04 23:54:16 -0600

    feat: adding image to mdx component

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,4 +1,5 @@
 import type { MDXComponents } from "mdx/types";
+import Image from "next/image";
 
 function CustomH1({
   children,
@@ -29,12 +30,29 @@ function CustomA({
   );
 }
 
+function CustomImg({
+  src,
+  alt,
+}: React.ImgHTMLAttributes<HTMLImageElement>): JSX.Element {
+  return (
+    <Image
+      src={src || ""}
+      alt={alt || ""}
+      width={800}
+      height={500}
+      className="mx-auto my-8 block rounded-lg shadow-md"
+      unoptimized={src?.startsWith("http")}
+    />
+  );
+}
+
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     h1: CustomH1,
     p: CustomP,
     li: CustomLi,
     a: CustomA,
+    img: CustomImg,
     wrapper: ({ children }) => (
       <div className="relative flex flex-1 flex-col overflow-hidden bg-gray-50 py-8 lg:py-12">
         <article className="prose prose-slate mx-auto px-4 lg:prose-lg md:mt-8 md:px-0">
